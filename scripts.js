94999d414aa525014e4dac8ff59e0b9d28b67d72
Elestin <daniel.brousalis@gmail.com>
2023-03-16 14:09:38 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,15 @@ const searchInput = document.getElementById('search-input');
 const searchResults = document.getElementById('search-results');
 const baseUrl = 'https://api.scryfall.com';
 
+// Declare dropdownContent here
+let dropdownContent;
+
 document.addEventListener("DOMContentLoaded", function () {
     const search = document.getElementById("search");
-    const dropdownContent = document.getElementById("dropdown-content");
     const gallery = document.getElementById("gallery");
+
+    // Initialize dropdownContent here
+    dropdownContent = document.getElementById("dropdown-content");
 
     search.addEventListener("input", function (event) {
         const query = event.target.value;
@@ -30,44 +35,41 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     });
 
-  function addToGallery(token) {
-    dropdownContent.style.display = "none";
+    function addToGallery(token) {
+        dropdownContent.style.display = "none";
 
-    const container = document.createElement("div");
-    container.className = "token-container";
-   
-}
+        const container = document.createElement("div");
+        container.className = "token-container";
 
-  const img = document.createElement("img");
-  img.src = token.image_uris.normal;
-  img.alt = `${token.name} (${token.power}/${token.toughness})`;
+        const img = document.createElement("img");
+        img.src = token.image_uris.normal;
+        img.alt = `${token.name} (${token.power}/${token.toughness})`;
 
-  const removeButton = document.createElement("button");
-  removeButton.textContent = "Remove";
-  removeButton.className = "remove-btn";
-  removeButton.addEventListener("click", function () {
-    gallery.removeChild(container);
-  });
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.className = "remove-btn";
+        removeButton.addEventListener("click", function () {
+            gallery.removeChild(container);
+        });
 
-  const duplicateButton = document.createElement("button");
-  duplicateButton.textContent = "Duplicate";
-  duplicateButton.className = "duplicate-btn";
-  duplicateButton.addEventListener("click", function () {
-    addToGallery(token);
-  });
+        const duplicateButton = document.createElement("button");
+        duplicateButton.textContent = "Duplicate";
+        duplicateButton.className = "duplicate-btn";
+        duplicateButton.addEventListener("click", function () {
+            addToGallery(token);
+        });
 
-  const tapButton = document.createElement("button");
-  tapButton.textContent = "Tap";
-  tapButton.className = "tap-btn";
-  tapButton.addEventListener("click", function () {
-    img.classList.toggle("tapped");
-  });
+        const tapButton = document.createElement("button");
+        tapButton.textContent = "Tap";
+        tapButton.className = "tap-btn";
+        tapButton.addEventListener("click", function () {
+            img.classList.toggle("tapped");
+        });
 
-  container.appendChild(img);
-  container.appendChild(removeButton);
-  container.appendChild(duplicateButton);
-  container.appendChild(tapButton);
-  gallery.appendChild(container);
-}
-
+        container.appendChild(img);
+        container.appendChild(removeButton);
+        container.appendChild(duplicateButton);
+        container.appendChild(tapButton);
+        gallery.appendChild(container);
+    }
 });
